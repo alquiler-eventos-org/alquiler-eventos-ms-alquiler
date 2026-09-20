73e78916049e19ad5804f327b9f8cb7e113d8116
Tobias-Huber-the <tobias.huber@fiuni.edu.py>
2026-09-20 03:08:57 -0300

    Proyecto base Spring Boot conectado a Supabase

---
+++ src/main/java/com/alquilereventos/alquiler/MsAlquilerApplication.java
@@ -0,0 +1,13 @@
+package com.alquilereventos.alquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAlquilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAlquilerApplication.class, args);
+	}
+
+}
